c1b70dfa1327149457f627bc1c36f03727b90f35
Harishan15 <harishan820@gmail.com>
2020-11-27 22:11:30 +0530

    added Book and Booklist Components

---
+++ src/components/Book.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Book extends Component {
+	state = {};
+	render() {
+		return <h2>Book Title</h2>;
+	}
+}
+
+export default Book;
---
+++ src/components/Booklist.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Book from "./Book";
+
+class Booklist extends Component {
+	state = {};
+	render() {
+		return (
+			<div>
+				<h1>NYTimes Best Selling Books</h1>
+				<Book />
+				<Book />
+				<Book />
+			</div>
+		);
+	}
+}
+
+export default Booklist;
