b3bffd6e4214bab8050e92cff565ae30c6f7c0ca
Harishan15 <harishan820@gmail.com>
2020-11-28 13:00:26 +0530

    new horizontal card style book list

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
 	return (
 		<div className="container-fluid px-0">
 			<Navbar />
-			<div className="ml-3 p-1">
+			<div className="ml-3 p-1 mt-3">
 				<h1 style={{ fontFamily: "Playfair Display", fontSize: "2rem" }}>
-					New York Times Best Sellers
+					New York Times Best Selling Books
 				</h1>
 			</div>
 			<BookList />
--- src/components/CustomBook.js
+++ src/components/CustomBook.js
@@ -10,8 +10,27 @@ const Book = ({
 	description,
 }) => {
 	return (
-		<div className="col d-flex justify-content-center">
-			<div
+		<div className="col-3 d-flex justify-content-center mt-3">
+			{/* <div className="col d-flex justify-content-center mt-3"> */}
+			<div className="card mb-3 p-0" style={{ maxWidth: 540 }}>
+				<div className="row no-gutters">
+					<div className="col-md-4 p-1">
+						<img src={imgUrl} className="card-img" alt={title} />
+					</div>
+					<div className="col-md-8">
+						<div className="card-body">
+							<h5 className="card-title">{title}</h5>
+							<h6 className="card-subtitle mb-2 text-muted">{contributor}</h6>
+
+							<p className="card-text">
+								<small className="text-muted">Last updated 3 mins ago</small>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* <div
 				className="card book-card mb-3"
 				style={{
 					width: "230px",
@@ -26,7 +45,7 @@ const Book = ({
 				/>
 				<div className="card-body">
 					<h5 className="card-title">{title}</h5>
-					{/* <p className="card-text">{contributor}</p> */}
+					<p className="card-text">{contributor}</p>
 					<h6 className="card-subtitle mb-2 text-muted">{contributor}</h6>
 
 					<p className="card-text" style={{ fontSize: "14px" }}>
@@ -36,7 +55,7 @@ const Book = ({
 						Buy on Amazon
 					</a>
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 };
